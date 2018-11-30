d8edf90ee53dd098ba38be7a339631baa79d7bcc
DESKTOP-7N73V42\fulls <fullsuper98@gmail.com>
2018-11-30 16:05:21 +0700

    "Suy't" Complete service

---
+++ src/grouptwo/quizexam/service/QuestioncategorieService.java
@@ -0,0 +1,159 @@
+package grouptwo.quizexam.service;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import grouptwo.quizexam.model.Questioncategorie;
+
+public class QuestioncategorieService extends BaseService {
+
+	public QuestioncategorieService() {
+		super();
+	}
+	public  boolean addQuestionCategory(String categoryName)
+	{
+		String sql="insert into questioncategories(CategoryName) values(?)";
+		List<Object> param= new ArrayList<>();
+		param.add(categoryName);
+
+			try {
+				return executeUpdate(sql,param);
+			} catch (SQLException e) {
+				System.out.println(e.getMessage());
+			}
+		return false;
+		
+		
+	}
+	public  List<Questioncategorie> getAllQuestionCategorie()
+	{
+		String sql="select * from questioncategories";
+		List<Questioncategorie> list=null;
+		Questioncategorie qsc=null;
+		try {
+			ResultSet rs=excuteQuery(sql);
+			list=new ArrayList<>();
+			
+			while(rs.next())
+			{
+				qsc=new Questioncategorie();
+				qsc.setId(rs.getInt(1));
+				qsc.setCategoryName(rs.getString(2));
+				list.add(qsc);
+			}
+		}
+		catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		return list;
+	}
+	public  Questioncategorie getQuestionCategorie(int id)
+	{
+		Questioncategorie qsc=new Questioncategorie();
+		String sql="select * from questioncategories where id=?";
+		List<Object> param= new ArrayList<>();
+		param.add(id);
+		try {
+			ResultSet rs=excuteQuery(sql,param);
+			while(rs.next())
+			{
+				qsc=new Questioncategorie();
+				qsc.setId(rs.getInt(1));
+				qsc.setCategoryName(rs.getString(2));
+			}
+		}
+		catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return qsc;
+	}
+	public  Questioncategorie getQuestionCategorie(String name)
+	{
+		Questioncategorie qsc=new Questioncategorie();
+		String sql="select * from questioncategories where CategoryName=?";
+		List<Object> param= new ArrayList<>();
+		param.add(name);
+		try {
+			ResultSet rs=excuteQuery(sql,param);
+			while(rs.next())
+			{
+				qsc=new Questioncategorie();
+				qsc.setId(rs.getInt(1));
+				qsc.setCategoryName(rs.getString(2));
+			}
+		}
+		catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return qsc;
+	}
+	public  boolean deleteQuestionCategorie(int id_QuestionCategorie)
+	{
+		String sql="delete from questioncategories where id=?";
+		List<Object> param= new ArrayList<>();
+		param.add(id_QuestionCategorie);
+		try {
+			return executeUpdate(sql, param);
+		}
+		catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return false;
+	}
+	public  boolean updateQuestionCategorie(int id_QuestionCategorie,String categoryName)
+	{
+		String sql="update questioncategories set CategoryName=? where id=?";
+		List<Object> param= new ArrayList<>();
+		param.add(categoryName);
+		param.add(id_QuestionCategorie);
+		try {
+			return executeUpdate(sql, param);
+		}
+		catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return false;
+	}
+	public  int getIdQuestitonCategory(String name)
+	{
+		int id=-1;
+		String sql="select id from questioncategories where CategoryName=?";
+		List<Object> param= new ArrayList<>();
+		param.add(name);
+		try {
+			ResultSet rs= excuteQuery(sql, param);
+			while(rs.next())
+			{
+				id=rs.getInt(1);
+			}
+		}
+		catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return id;
+	}
+	public  String getNameQuestionCategory(int id_Categorie)
+	{
+		String Name=null;
+		String sql="select CategoryName from questioncategories where id=?";
+		List<Object> param= new ArrayList<>();
+		param.add(id_Categorie);
+		try {
+			ResultSet rs= excuteQuery(sql, param);
+			while(rs.next())
+			{
+				Name=rs.getString(1);
+			}
+		}
+		catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return Name;
+	}
+
+
+}
--- src/grouptwo/quizexam/service/RoleuserService.java
+++ src/grouptwo/quizexam/service/RoleuserService.java
@@ -5,14 +5,18 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import grouptwo.quizexam.model.Questioncategorie;
+
 import grouptwo.quizexam.model.Roleuser;
+
+
+
 
 public class RoleuserService extends BaseService {
 
 	public RoleuserService() {
 		super();
 	}
+
 	public  boolean addRoleuser(String RoleuserName)
 	{
 		String sql="insert into roleusers(RoLeName) values(?)";
@@ -152,5 +156,92 @@ public class RoleuserService extends BaseService {
 			System.out.println(e.getMessage());
 		}
 		return Name;
+	}
+
+	public List<Roleuser> getAllRoleUser() {
+		String query = "Select * from roleuser";
+		List<Roleuser> lstRoleUser = new ArrayList<>();
+
+		try {
+			ResultSet rs = excuteQuery(query);
+			while (rs.next()) {
+				Roleuser roleUser = new Roleuser(
+						rs.getInt("Id"), 
+						rs.getString("RoleName"));
+				lstRoleUser.add(roleUser);
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			return null;
+		}
+		return lstRoleUser;
+	}
+
+	public Roleuser getRoleUserById(int id) {
+		String query = "Select * from roleusers where Id = " +id;
+		try
+		{
+			ResultSet rs = excuteQuery(query);
+			Roleuser roleuser = new Roleuser(
+					rs.getString("RoleName"));
+			return roleuser;
+		}
+		catch(SQLException e)
+		{
+			System.out.println(e.getMessage());
+
+		}
+		return null;
+	}
+
+	public Roleuser getRoleUserByName(String subject) {
+		return null;
+	}
+
+	public boolean deleteRoleUser(int id) {
+		String query="Delete from roleusers where Id=?";
+		List<Object> params= new ArrayList<>();
+		params.add(id);
+		try {
+			boolean action = executeUpdate(query, params);
+			return action;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	public boolean updateRoleUser(Roleuser roleUser) {
+
+		String query ="update roleusers set "
+				+ "RoleName=?,"
+				+ "Where Id= ?";
+		List<Object> params= new ArrayList<>();
+		params.add(roleUser.getRoleName());
+		params.add(roleUser.getId());
+		try {
+			boolean action = executeUpdate(query, params);
+			return action;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
+
+	}
+
+	public boolean addRoleUser(Roleuser roleUser) {
+		String query="Insert into roleusers (RoleName)"
+				+"values (?)";
+	List<Object> params= new ArrayList<>();
+	params.add(roleUser.getRoleName());
+	try {
+		boolean action = executeUpdate(query, params);
+		return action;
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+	return false;
+
+
 	}
 }
